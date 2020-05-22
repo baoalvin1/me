5f5e5ab56b7ce3e8ce7dab3be45049576f129d42
Alvin Bao <alvinbao@alvins-mbp.lan>
2020-05-22 16:54:57 -0400

    changed title

--- src/components/about.js
+++ src/components/about.js
@@ -93,7 +93,7 @@ export default class About extends Component {
                     <h2 className="mb-4"> Work Experience</h2>
                     <div className="exp-wrap py-4">
                       <div className="desc">
-                        <h4>Pagarba<span> - Software Engineer Intern</span> </h4>
+                        <h4>Pagarba<span> - Software Engineering Intern</span> </h4>
                         <p className="location">Marietta, Georgia</p>
                       </div>
                       <div className="year">
@@ -119,7 +119,7 @@ export default class About extends Component {
                       </div>
                     </div>
                   </div>
-                  
+
                   <div className="exp mt-5 ftco-animate">
                     <h2 className="mb-4"> Clubs and Leadership</h2>
                     <div className="exp-wrap py-4">
